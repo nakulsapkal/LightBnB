4608d8701ab190aca06274cc8ba82a7f1152b9e5
Nakul Sapkal <nakul.sapkal@gmail.com>
2021-06-23 23:29:10 +0000

    1_queries

---
+++ 1_queries/4_Most_Visited_Cities.sql
@@ -0,0 +1,8 @@
+-- Get a list of the most visited cities.
+
+
+SELECT city,count(reservations.*) AS total_reservations
+FROM properties
+JOIN reservations ON property_id = properties.id
+GROUP BY city
+ORDER BY total_reservations DESC;
